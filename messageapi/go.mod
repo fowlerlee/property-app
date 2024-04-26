00ed09c6d189f8c0095a0134d2996d8a892c042c
Lee Fowler <fowler.lee8@gmail.com>
2024-04-26 10:05:19 +0200

    remove gobackend add messageapi

---
+++ messageapi/go.mod
@@ -0,0 +1,11 @@
+module github.com/fowlerlee/property-app/messageapi
+
+go 1.22.0
+
+require (
+	github.com/golang-jwt/jwt/v4 v4.5.0
+	github.com/gorilla/mux v1.8.1
+	github.com/lib/pq v1.10.9
+	github.com/rabbitmq/amqp091-go v1.9.0
+	golang.org/x/crypto v0.22.0
+)
---
+++ messageapi/rabbit.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
+func CreateRabbitChannel() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+}
