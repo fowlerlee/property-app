8518bd8a931a85c1eee9607eb47bf9d2a66f5a20
Lee Fowler <fowler.lee8@gmail.com>
2024-05-06 00:47:14 +0200

    add grpc and rabbitmq
    Signed-off-by: Lee Fowler <fowler.lee8@gmail.com>

--- messageapi/client/client.go
+++ messageapi/client/client.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	api "github.com/fowlerlee/property-app/messageapi/proto"
+	amqp "github.com/rabbitmq/amqp091-go"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
@@ -14,12 +15,6 @@ import (
 const (
 	port = ":3200"
 )
-
-func failOnError(err error, msg string) {
-	if err != nil {
-		log.Fatalf("%s: %s", msg, err)
-	}
-}
 
 const (
 	defaultLog = "logEmpty"
@@ -29,6 +24,24 @@ var (
 	addr = flag.String("addr", "localhost:3200", "the address to connect to")
 	name = flag.String("name", defaultLog, "Log message")
 )
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
+func CreateRabbitChannel() *amqp.Channel {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+
+	defer conn.Close()
+	ch, err := conn.Channel()
+	if err != nil {
+		failOnError(err, "Failed to open a channel")
+	}
+	return ch
+}
 
 func main() {
 
@@ -47,12 +60,39 @@ func main() {
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
 	}
-	log.Printf("Greeting: %s", r.GetMessage())
+	log.Printf("RPC message: %s", r.GetRecord().Value)
 
-	r, err = c.Produce(ctx, &api.ProduceRequest{Record: })
+	connCh, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+
+	defer conn.Close()
+	ch, err := connCh.Channel()
 	if err != nil {
-		log.Fatalf("could not greet: %v", err)
+		failOnError(err, "Failed to open a channel")
 	}
-	log.Printf("Greeting: %s", r.GetMessage())
 
+	q, err := ch.QueueDeclare(
+		"message", // name
+		false,     // durable
+		false,     // delete when unused
+		false,     // exclusive
+		false,     // no-wait
+		nil,       // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+	ctxChannel, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	body := "Hello World!"
+	err = ch.PublishWithContext(ctxChannel,
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
+	log.Printf(" [x] Sent %s\n", body)
 }
--- messageapi/server/server.go
+++ messageapi/server/server.go
@@ -3,10 +3,9 @@ package main
 import (
 	"context"
 	"log"
-
 	"net"
-
 	api "github.com/fowlerlee/property-app/messageapi/proto"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 const (
@@ -33,13 +32,6 @@ func newgrpcServer(config *Config) (srv *grpcServer, err error) {
 	return srv, nil
 }
 
-func failOnError(err error, msg string) {
-	if err != nil {
-		log.Fatalf("%s: %s", msg, err)
-	}
-}
-
-// START: request_response
 func (s *grpcServer) Produce(ctx context.Context, req *api.ProduceRequest) (
 	*api.ProduceResponse, error) {
 	offset, err := s.CommitLog.Append(req.Record)
@@ -58,8 +50,11 @@ func (s *grpcServer) Consume(ctx context.Context, req *api.ConsumeRequest) (
 	return &api.ConsumeResponse{Record: record}, nil
 }
 
-// END: consume_stream
-// END: stream
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
 
 func main() {
 	// conn, err := grpc.Dial("localhost"+port, grpc.WithTransportCredentials(insecure.NewCredentials()))
@@ -72,5 +67,45 @@ func main() {
 	defer listener.Close()
 
 	newgrpcServer(&Config{})
+
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"message", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	var forever chan struct{}
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
 
 }
